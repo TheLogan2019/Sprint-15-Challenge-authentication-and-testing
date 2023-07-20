c23b7194addf096b3b828608563c617bc851e63d
Logan <thelogan2019@gmail.com>
2023-07-20 15:13:05 -0500

    Middleware established

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,48 @@
+const authCom = require("./auth-model");
+
+const checkForExistingUser = async (req, res, next) => {
+  const [user] = await authCom.findByUsername(req.body.username);
+  console.log(user);
+  try {
+    if (user) {
+      next({ status: 422, message: "Username taken" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const validateUsername = async (req, res, next) => {
+  const [user] = await authCom.findByUsername(req.body.username);
+  console.log(user);
+  try {
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      next({ status: 401, message: "Invalid credentials" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const validateCredentials = (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(404).json({
+      message: "username and password required",
+    });
+    return;
+  }
+  next();
+};
+
+module.exports = {
+  checkForExistingUser,
+  validateUsername,
+  validateCredentials,
+};
